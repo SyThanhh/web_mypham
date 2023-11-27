ea034d4cf4587ae0e04c0528efcbfe9690a99564
Thanh27 <95127658+SyThanhh@users.noreply.github.com>
2023-11-27 14:53:01 +0700

    code dang nhap

--- js/redexDangNhap.js
+++ js/redexDangNhap.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {
+
+    const email = $('#email');
+    const pass = $('#pass');
+
+    const user = JSON.parse(localStorage.getItem('user')) || {};
+    let sdt = user.sdt;
+    let password = user.password;
+    
     function checkTenDangNhap() {
         let dangNhap = $("#txtDN").val();
         if (dangNhap.trim() == "") {
             $('#errDN').html("(*) Bắt buộc nhập");
             return false;
-        } else {
+        } else  if(sdt!= dangNhap) {
+            $('#errDN').html("(*) Sdt hoặc email bị sai!");
+            return false;
+        }
+        else {
             $('#errDN').html("");
             return true;
         }
+       
 
     }
     $("#txtDN").blur(function() {
@@ -19,7 +32,11 @@ $(document).ready(function() {
         if (MK.trim() == "") {
             $('#errMK').html("(*) Bắc buộc nhập");
             return false;
-        } else {
+        } else if(password != MK) {
+            $('#errDN').html("(*) Sdt hoặc email bị sai!");
+            return false;
+        } else
+        {
             $('#errMK').html("");
             return true;
         }
@@ -30,4 +47,11 @@ $(document).ready(function() {
         checkMK();
     });
 
+
+    $('#send2').click(function(event) {
+        if(!checkTenDangNhap() && !checkMK()) {
+            alert('Tài khoản không chính xác!');
+            event.preventDefault(); 
+        }
+    })
 });
